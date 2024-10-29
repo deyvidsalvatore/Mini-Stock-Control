c3a8bd25f4507bd2763fe64f521a5195a6e3b978
Deyvid Santos <deyvidsantosdasilva2002@gmail.com>
2024-10-29 19:45:24 -0300

    feat: Swagger Documentation

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import routes from './routes';
 
+import swaggerUi from "swagger-ui-express";
+import swaggerOutput from "./swagger_output.json";
+
 const app = express();
 const port = 3000;
 
@@ -10,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/", routes);
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerOutput));
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
