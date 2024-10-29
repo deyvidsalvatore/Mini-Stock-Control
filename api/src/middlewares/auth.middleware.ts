1b20a8dc953b02a38c0070d0a993abf751586709
Deyvid Santos <deyvidsantosdasilva2002@gmail.com>
2024-10-29 17:47:26 -0300

    chore: auth middleware

--- api/src/middlewares/auth.middleware.ts
+++ api/src/middlewares/auth.middleware.ts
@@ -1,22 +1,25 @@
 import { NextFunction, Request, Response } from 'express';
-import { verify } from "jsonwebtoken";
+import { verify } from 'jsonwebtoken';
 import { Payload } from '../models/user/auth/Payload';
 
-export function isAuthenticated(request: Request, response: Response, next: NextFunction) {
-    const authToken = request.headers.authorization;
-    
+export function isAuthenticated(req: Request, res: Response, next: NextFunction): void {
+    const authToken = req.headers.authorization;
+
     if (!authToken) {
-        return response.status(401).end();
+        res.status(401).end();
     }
 
-    const [_, token] = authToken.split(" ");
+    const token = authToken.split(" ")[1];
+
+    if (!token) {
+        res.status(401).end();
+    }
 
     try {
         const { sub } = verify(token, process.env.JWT_SECRET as string) as Payload;
-        request.user_id = sub;
-        return next();
+        req.user_id = sub;
+        next();
     } catch (error) {
-        return response.status(401).end();
+        res.status(401).end();
     }
-    
 }
