5332d5c5c8d50345b124c7bf2683c6e34ec58de2
Deyvid Santos <deyvidsantosdasilva2002@gmail.com>
2024-10-30 13:54:54 -0300

    feat: Dynamic Form Component

---
+++ ministock-ui/src/app/shared/dynamic-form/dynamic-form.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { QuestionBase } from '../generics/questions/question-base.generic';
+import { QuestionControlService } from '../services/question-control.service';
+import { DynamicFormQuestionComponent } from './dynamic-form-question/dynamic-form-question.component';
+
+@Component({
+  selector: 'app-dynamic-form',
+  templateUrl: './dynamic-form.component.html',
+  styleUrl: './dynamic-form.component.scss',
+  standalone: true,
+  imports: [CommonModule, DynamicFormQuestionComponent, ReactiveFormsModule],
+  providers: [QuestionControlService]
+})
+export class DynamicFormComponent implements OnInit {
+  @Input() questions: QuestionBase<string>[] | null = [];
+  form!: FormGroup;
+  payLoad = '';
+  myQcs!: QuestionControlService;
+
+  constructor(private qcs: QuestionControlService) {
+  }
+
+  ngOnInit(): void {
+    this.form = this.qcs.toFormGroup(this.questions as QuestionBase<string>[]);
+  }
+
+  onSubmit(): void {
+    this.payLoad = JSON.stringify(this.form.getRawValue());
+  }
+
+}
