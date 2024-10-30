d34a7c25ae7fda9de0b30ef67ac8d77232784917
Deyvid Santos <deyvidsantosdasilva2002@gmail.com>
2024-10-30 18:39:06 -0300

    feat: Home page

---
+++ ministock-ui/src/app/modules/home/home.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { LoginFormComponent } from './shared/login-form/login-form.component';
+import { DynamicFormComponent } from "../../shared/dynamic-form/dynamic-form.component";
+import { CardModule } from 'primeng/card';
+import { SignupFormComponent } from './shared/signup-form/signup-form.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    LoginFormComponent,
+    SignupFormComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    DynamicFormComponent,
+    CardModule
+]
+})
+export class HomeModule { }
