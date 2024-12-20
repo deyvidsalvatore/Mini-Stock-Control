113a45d03b1ae3ff26718c44a5e3cebead79ff14
Deyvid Santos <deyvidsantosdasilva2002@gmail.com>
2024-12-20 14:42:18 -0300

    feat: Category Service

---
+++ ministock-ui/src/app/core/services/categories.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { AllCategoriesResponse } from '../../shared/interfaces/products/responses/all-categories.response';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoriesService {
+  private API_URL = environment.apiUrl;
+  private JWT_TOKEN = this.cookie.get('USER_INFO');
+  private HTTP_OPTIONS = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.JWT_TOKEN}`,
+    }),
+  };
+
+  constructor(private http: HttpClient, private cookie: CookieService) {}
+
+  getAllCategories(): Observable<AllCategoriesResponse[]> {
+    return this.http.get<AllCategoriesResponse[]>(`${this.API_URL}/categories`, this.HTTP_OPTIONS);
+  }
+}
---
+++ ministock-ui/src/app/shared/interfaces/products/responses/all-categories.response.ts
@@ -0,0 +1,4 @@
+export interface AllCategoriesResponse {
+  id: string;
+  name: string;
+}
